4ace6d885bdd1d42fa03788555d23e725fa96c0e
Selby Baidjnath <selby@serubii.com>
2024-07-30 02:52:33 -0300

    settingsUi init

---
+++ lib/features/settings/settings_ui.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SettingsUi extends HookConsumerWidget {
+  const SettingsUi({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ;
+  }
+}
